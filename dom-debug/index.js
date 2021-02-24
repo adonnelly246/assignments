3b44a5188f92007d40a5bbc5d355d3d4baf58045
Alexandra Donnelly <adonnelly246@gmail.com>
2021-02-24 13:12:32 -0600

    incresed font size on css and fixed eventlistener to change background color of SubItem

--- dom-debug/index.js
+++ dom-debug/index.js
@@ -1,51 +1,44 @@
 
-var list = document.getElementById("list");
-
-//const main = document.getElementsByClassName("main");
 //adds the next element on page using createsubitem
 document.getElementById("add").addEventListener("click", function(e){
-    var input = document.getElementById("input");
- 
-
-    const userInput = input.innerHTML;
-
-    
-    const subItem = createSubItem(inputVal)
+    const subItem = createSubItem(e)
     document.getElementById("list").appendChild(subItem)
-});
-
+ 
 //creates dropdown menu for the color selection
 function createDropDown(Colors){
-    var dropDown = document.createElement("select");
-    var Colors = ["Select a Color", "red", "blue", "green"];
-    
-    //loops through the Colors arr 
+var dropDown = document.createElement("select");
+var Colors = [ "red", "blue", "green"];
+
+    //loops through the Colors arr and adds dropdown choices
     for ( var i = 0; i < Colors.length; i++){
         var col = Colors[i];
         var option = document.createElement("option") 
         option.textContent = col;
         option.value = col;
-        dropDown.append(option);
-    }
-    
-    dropDown.addEventListener("onchange", function(e){
+        dropDown.append(option);   
        
-    })
-    return dropDown
+        dropDown.addEventListener("change", function(e){
+            //changes backgroundcolor based on dropdown choice
+            subItem.style.backgroundColor = e.target.value
+     })
+    }
+    return dropDown  
 }
 
 //creates subitem
 function createSubItem(e){
     const subItem = document.createElement("div")
-    var subItemValue = document.getElementById("input")
+    var subItemValue = document.getElementById("input").value
     subItem.textContent = subItemValue
     const dropDown = createDropDown()
     subItem.appendChild(dropDown)
     subItem.setAttribute("class", "subItem")
-
-    return subItem
-
-    
+    return subItem   
 }
+});
+
+
+
+
 
 
