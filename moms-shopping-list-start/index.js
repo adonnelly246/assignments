8e708a58c9cc3c377faec219fc417624688df713
Alexandra Donnelly <adonnelly246@gmail.com>
2021-02-25 17:54:31 -0600

    added input form and add buttons functionality

--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -4,6 +4,23 @@ form.addEventListener('submit', function(event){
     event.preventDefault(); 
     const newItem = form.newInput.value
     form.newInput.value = ""
+    console.log(newItem) //logs the new input to console-check if went through
+    
+    //creates new li to the list
+   const list = document.getElementById("list")
+     var textnode = document.createTextNode(newItem)   
+    list.appendChild(textnode);
+     
+    //adds the edit and X buttons to the list item
+     var editBtn = document.createElement('button') 
+     editBtn.innerText = "edit"
+     editBtn.style.width = "40px"
+     list.appendChild(editBtn)
+     var xBtn = document.createElement('button') 
+     xBtn.innerText = "X"
+     xBtn.style.width = "25px"
+     list.appendChild(xBtn)
+                         
+    //need to center buttons and add padding to new list item word
 
-    console.log(newItem)
 });
