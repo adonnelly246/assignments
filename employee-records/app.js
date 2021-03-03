60d7c585f579c7a153c3ea60885d5cfa4f60d0aa
Alexandra Donnelly <adonnelly246@gmail.com>
2021-03-03 14:59:41 -0600

    final commit project complete

--- employee-records/app.js
+++ employee-records/app.js
@@ -1,25 +1,26 @@
 //create an empty array named employees
-
+var employees = []
 // create an Employee constructor with the first three attributes defined at the time of instantiation and the fourth will be defaulted to "Full Time".
-
 //This constructor will also have a method called printEmployeeForm that prints the employee's information to the console.
+function Employees(name, title, salary, printEmployeeForm){
+    this.name = name;
+    this.title = title;
+    this.salary = salary;
+    this.status = "Full Time";
+    
+this.printEmployeeForm = function () {
+    console.log("Name: " + this.name + ", " + "Title: "+ this.title, + ", " + "Salary: "+ this.salary + ", " + "Status: " + this.status)
+  };
+}
+
+ var employeeOne = new Employees( "Sarrah Brown", "Director of Marketing", "90,000/year")
+var emplyeeTwo = new Employees ( "George Hammon", "Instructor", "75,000/year")
+
+employeeOne.printEmployeeForm()
+emplyeeTwo.printEmployeeForm()
 
 //(e.g. "Name: Bob, Job Title: V School Instructor, Salary: $3000/hour, Status: Part time")
 
 
 
 
-
-//Here's a refresher for the constructor syntax:
-
-function Car (year, make, model) {
-    // use the "this" keyword to reference each object 
-    // that is created from this constructor
-    this.year = year;
-    this.make = make;
-    this.model = model;
-}
-
-// then we "instantiate" (create a new object from) our constructor with the "new" keyword and pass in the parameters:
-
-var myTacoma = new Car(2015, "Toyota", "Tacoma");
