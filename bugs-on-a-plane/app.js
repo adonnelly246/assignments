56f149cfc0e7079fa3d1cd19d27a2af1f9528192
Alexandra Donnelly <adonnelly246@gmail.com>
2021-03-04 14:20:08 -0600

    found several bugs, just needs submit button

--- bugs-on-a-plane/app.js
+++ bugs-on-a-plane/app.js
@@ -1,27 +1,33 @@
-var form = document.getElementByID("arline-form");
-var submit = document.getElementByID(submit);
-var query = document.querySelector;
+const form = document.airlineForm;
+//const submit = document.getElementByID(submit);
+//var query = document.querySelector();
 
-function formAlert{} {
-    var firstName = form.elements["firstName"].value;
-    var lastName = form.elements["lastName"].value;
-    var age = form.elements["age"].value;
-    var gender = form.elements["gender"].value;
-    var location = form.elements["travel-location"].value;
-    var diet = {};
-    if (form.elements['vegan'].checked) {
-        var diet.pop(document.getElementById("vegan").value);
+  
+
+form.addEventListener('submit', function (event){
+    event.preventDefault()
+    let firstName = form.firstName.value;
+    let lastName = form.lastName.value;
+    let age = form.age.value;
+    let gender = form.gender.value;
+    const location = form.travelLocation.value;
+    let diet = [];
+
+    //if form selection = 'vegan' , 'gluten-free', 'paleo', add to diet array
+    if (form.vegan.checked == true) {
+        diet.push(form.vegan.value);
     }
-    if (form.elements['gluten'].checked) {
-        diet.push(document.getElementById('gluten').value);
+    if (form.gluten.checked == true) {
+        diet.push(form.gluten.value);
     }
-    if (form.elements['paleo'].checked) {
-        diet.push(document.getElementById('paleo').value);
+    if (form.paleo.checked == true) {
+        diet.push(form.paleo.value);
     }
 
+      
+    //alert box 
 
-    alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nAge: " + age + "\nGender: " + gender + "\nTravel Location: " + location + "\nDiet: " + diet + "\nAwesome, now if you die, it won't be an accident..");
-}
+    alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nAge: " + age + "\nGender: " + gender + "\nTravel Location: " + location + "\nDiet: " + diet + "\nAwesome, now if you die, it won't be an accident..")
+  
+});
 
-
-submit.addEventListener("click", formalert);
