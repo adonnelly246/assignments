69b442896e431e6a74f2ee3e20e95c5b98c2a78f
Alexandra Donnelly <adonnelly246@gmail.com>
2021-04-01 19:00:09 -0500

    worked on adding a timmer. needs debugging/testing

--- level-2/speed-clicker/app.js
+++ level-2/speed-clicker/app.js
@@ -1,14 +1,15 @@
 var count = 0
 let current = document.getElementById("current")
 let previous = document.getElementById("previous")
-
+let timer = document.getElementById("timer")
 document.getElementById("any").addEventListener('click', e =>{
+ 
     count = parseInt(count) 
     count += 1
     current.textContent = "Current Number of Clicks: " + count
     //holds value on refresh
   localStorage.setItem(previous, count)
-  
+
 })
 
  
@@ -16,4 +17,9 @@ document.getElementById("any").addEventListener('click', e =>{
 
 
   //bonus add timer
-  
+  setInterval(()=>{
+    //get timeLeft
+
+    //display the timer counting down
+    timer.textContent = timer.textContent + timeLeft
+}, 10000)
