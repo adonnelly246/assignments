3663bafd546d6f3e1f9ae9e350122875dbb92358
Alexandra Donnelly <adonnelly246@gmail.com>
2021-04-05 22:39:13 -0500

    finalcommit

--- level-2/pokemon-list/app.js
+++ level-2/pokemon-list/app.js
@@ -1,14 +1,13 @@
 const xhr = new XMLHttpRequest()
 xhr.open("GET", "https://api.vschool.io/pokemon", true)
-xhr.send()
-
+xhr.send(body)
 
 xhr.onreadystatechange = function(){
     if(xhr.readyState === 4 && xhr.status === 200){
-        const response = JSON.parse(xhr.responseText)
-        // const returnVal = JSON.parse(response)
-        console.log(response)
-        displayHtml(response)
+        const response = xhr.responseText
+        const returnVal = JSON.parse(response)
+       console.log(returnVal.objects[0].pokemon)
+        displayHtml(returnVal.objects[0].pokemon)
     }
   
 }
@@ -16,7 +15,7 @@ xhr.onreadystatechange = function(){
 function displayHtml(arr){
     for(let i = 0; i < arr.length; i++){
     const h1 = document.createElement('h1')
-    h1.textContent = arr[i]
+    h1.textContent =arr[i].name
     document.body.appendChild(h1)
     }
 }
