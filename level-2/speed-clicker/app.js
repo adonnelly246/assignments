73a849a38ee57482bad06d5c283edaede88f11e9
Alexandra Donnelly <adonnelly246@gmail.com>
2021-04-13 21:59:56 -0500

    added interval stopwatch

--- level-2/speed-clicker/app.js
+++ level-2/speed-clicker/app.js
@@ -2,13 +2,14 @@ var count = 0
 let current = document.getElementById("current")
 let previous = document.getElementById("previous")
 let timer = document.getElementById("timer")
+
 document.getElementById("any").addEventListener('click', e =>{
- 
+  startTimer(1000,timer)
     count = parseInt(count) 
     count += 1
     current.textContent = "Current Number of Clicks: " + count
     //holds value on refresh
-  localStorage.setItem(previous, count)
+     localStorage.setItem(previous, count)
 
 })
 
@@ -16,10 +17,23 @@ document.getElementById("any").addEventListener('click', e =>{
   previous.textContent = "Previous Number of Clicks: " + localStorage.getItem(previous)
 
 
-  //bonus add timer
-  setInterval(()=>{
-    //get timeLeft
+//   function startTimer(duration, display) {
+//     let stopWatch = duration, minutes, seconds;
+//     setInterval(function () {
+//         minutes = parseInt(stopWatch / 60, 10);
+//         seconds = parseInt(stopWatch % 60, 10);
 
-    //display the timer counting down
-    timer.textContent = timer.textContent + timeLeft
-}, 10000)
+//         minutes = minutes < 1 ? "0" : minutes;
+//         seconds = seconds < 10 ? "0" + seconds : seconds;
+
+//         display.textContent = minutes + ":" + seconds;
+
+//         if (stopWatch <= 0) {
+//             stopWatch = duration;
+
+//         }
+//     }, 1001);
+// }
+
+
+ 
