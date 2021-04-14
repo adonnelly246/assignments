0f95018d644d1c0b69c3cdabdea7fd086ab1e1a0
Alexandra Donnelly <adonnelly246@gmail.com>
2021-04-13 22:02:17 -0500

    fixed the time interval/clear interval

--- level-2/princess-in-another-castle/app.js
+++ level-2/princess-in-another-castle/app.js
@@ -3,88 +3,81 @@ class Player {
     gameActive = true
     constructor(name){
         this.name = name
-        this.totalCoins = [0]
+        this.totalCoins 
         this.status = "small"
         this.hasStar = false
        
     }
-        //use namePicked to pass in "mario" or "luigi" //----------------------may not need/redundant
-    //    setName(namePicked){
-    //        if (namePicked=== "Mario"){
-    //            this.name = "Mario"
-    //        }
-    //        else if(namePicked === "Luigi"){
-    //         this.name = "Luigi"
-    //        }
-          
-    //     }
+        //use namePicked to pass in "mario" or "luigi" 
+       setName(namePicked){
+        this.name = namePicked
+        }
         gotHit(){
-                if (hasStar===true){
-                    this.status = "BIG"
+                if (this.hasStar===true){
+                    console.log("Enemy has been killed")
+                   this.status = "BIG"
+                    this.hasStar = false
                 }
                 else{
-                    this.status = "Dead"
-                    this.gameActive = false
                     console.log("you died")
+                   this.status = "Dead"
+                    this.gameActive = false
+                    
                 }
         }
       gotPowerUp(){
-            this.status = "Powered up"
+        console.log("You got a Star!")
+          this.status = "Powered up"
             this.hasStar = true
-            console.log("You got a Star!")
+            
         }
        
-     addCoin(){
-            this.totalCoins = + 1
+     addCoin(totalCoins){
+        console.log("coin+")
+      totalCoins=  this.totalCoins += 1
+        
         }
-      print() {console.log(`Name: ${this.name} \n Total Coins: ${this.totalCoins} \n Status:${this.status}`)}
+
+      print(){console.log(`Name: ${this.name} \n Total Coins: ${this.totalCoins} \n Status:${this.status}`)}
  
-
-
 }
 
 // put the random range function inside a setInterval function that ends after gameActive === false
- 
-setInterval(() => {
+let intervalFunc = setInterval(() => {
 
-    console.log("intervalstart")
+    //use class to create an object
+    let player1 = new Player("Mario")
+    // let player2 = new Player("Luigi")
+    
+ //call print method on object
+ player1.print()    
+ // player2.print()
+
   function randRange(min,max)  {
-      let randNum = Math.floor(Math.random() * (max-min)+ min)
-      return randNum  
-  };
- 
- let rand = randRange(0,3)
- console.log(rand)
-  if (this.gameActive){
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  if(player1.gameActive === true){
+    let rand = randRange(0,3)
      if(rand === 0){
-        gotHit()
-        console.log("hit")
-        setInterval()
+        player1.gotHit()
      }
      else if(rand=== 1){
-        gotPowerUp()
-        console.log("powerup")
-        setInterval()
+        player1.gotPowerUp()
      }
-     if (rand === 2) {
-        addCoin()
-      console.log("coin+")
+     else if (rand === 2) {
+        player1.addCoin(player1.totalCoins)
+    
      } 
   }
-     else if(this.gameActive === false){
-         console.log("end")
-     }
- 
-    
-       
-    //use class to create an object
- const player1 = new Player("Mario", "Luigi")
- const player2 = new Player ("Luigi")
- //call print method on object
- player2.print()         //-------------------------- setName
-  
- }, 3000);
 
+  if(player1.gameActive === false){
+        //interval stops
+        clearInterval(intervalFunc)
+     }
+         
+     
+ }, 5000);
 
 
 
