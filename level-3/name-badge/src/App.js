41689169adfe6f464996c339662ad48141b0723a
Alexandra Donnelly <adonnelly246@gmail.com>
2021-06-07 15:01:30 -0500

    fixed clear form and created a reversed map to show newly added badge first

--- level-3/name-badge/src/App.js
+++ level-3/name-badge/src/App.js
@@ -1,85 +1,153 @@
 import React, {Component} from 'react'
 import NameBadge from "./NameBadge"
+import styles from "./styles.module.css"
 
 class App extends Component{
   
   state={
+
     firstName: "",
     lastName: "",
     email: "",
     birthPlace:"",
     phone: [],
     food: "",
-    badges: []
+    about: "",
+    badgeArr: []
   }
   handleChange =(e)=>{
-    const {info, value} = e.target
+    const {name, value} = e.target
     this.setState({
-        [info]: value
+        [name]: value
+
     })
+    //check length of input >= 3
+    if(value.length < 3) {
+      alert('Your text is less than 3 characters');
+   }
+//validate phone number has only numbers
   }
+
   handleSubmit = (e)=>{
-    e.preventDefault()
-    //clear old data/badge
+    e.preventDefault();
+  
     //add new badge
     this.setState(prevState => {
+     
+ 
 
         return{
-           badges: [] 
+          firstName: "",
+          lastName: "",
+          email: "",
+          birthPlace: "",
+          phone:"",
+          food: "",
+          about: "",
+
+          badgeArr: [
+            ...prevState.badgeArr, {  firstName: prevState.firstName,
+              lastName: prevState.lastName,
+              email: prevState.email,
+              birthPlace: prevState.birthPlace,
+              phone: prevState.phone,
+              food: prevState.food,
+              about: prevState.about
+            }
+          ]  
         }
+        
     })
+     
   }
 
+ 
 
   render(){
-      //mapp
+    let reversedBadges = []
+    const badges = this.state.badgeArr.map((badge => reversedBadges.unshift(badge)))
+      const displayedBadges = reversedBadges.map((badge)=>{ 
+        return <NameBadge key={this.state.id}  {...badge} />})
+      
+      
+    
       return(
-        <div>
-        <form>
-        <input    type="text" 
+        <div >
+            
+        <form onSubmit={this.handleSubmit} class={styles.mainContainer}>
+       
+          <div >
+          <input
+                style={{position: "absolute", top: "30px", left:"80px", padding:"5px", border: "solid 2px"}}
+                  type="text" 
                   value={this.state.firstName} 
                   name="firstName" 
                   placeholder="First Name" 
-                  onChange={this.handleChange}></input>
+                  onChange={this.handleChange}
+                 ></input>
 
-        <input    type="text" 
+        <input    
+                  style={{position: "absolute", top: "30px", left:"355px", 
+                           padding:"5px", border: "solid 2px"}}
+                  type="text" 
                   value={this.state.lastName} 
                   name="lastName" 
                   placeholder="Last Name" 
-                  onChange={this.handleChange}></input>
+                  onChange={this.handleChange}
+                 ></input>
 
-        <input    type="text" 
+        <input    
+                   style={{position: "absolute", top: "75px", left:"80px",
+                           padding:"5px", border: "solid 2px"}}
+                  type="text" 
                   value={this.state.email} 
                   name="email" 
                   placeholder="email" 
-                  onChange={this.handleChange}></input>
+                  onChange={this.handleChange}
+                 ></input>
 
-        <input    type="text" 
+        <input    
+                 style={{position: "absolute", top: "75px", left:"355px",
+                         padding:"5px", border: "solid 2px"}}
+                  type="text" 
                   value={this.state.birthPlace} 
                   name="birthPlace" 
                   placeholder="Place of Birth" 
-                  onChange={this.handleChange}></input>
+                  onChange={this.handleChange}
+                ></input>
 
-       <input    type="number" 
+       <input    
+                 style={{position: "absolute", top: "115px", left:"80px",
+                          padding:"5px", border: "solid 2px"}}
+                  type="number" 
                   value={this.state.phone} 
                   name="phone" 
                   placeholder="Phone Number" 
-                  onChange={this.handleChange}></input>
+                  onChange={this.handleChange}
+                  ></input>
 
-      <input    type="text" 
+      <input      
+                   style={{position: "absolute", top: "115px", left:"355px",
+                            padding:"5px", border: "solid 2px"}}
+                  type="text" 
                   value={this.state.food} 
                   name="food" 
                   placeholder="Favorite Food" 
-                  onChange={this.handleChange}></input>                       
-
-              <textarea 
-                  value={"Tell us about yourself"}
+                  onChange={this.handleChange}
+                 ></input>                       
+          </div>
+          <textarea 
+                  style={{position: "absolute", top: "155px", left:"150px", width: "250px", height: "60px",
+                          padding:"5px", border: "solid 2px" }}
+                  value={this.state.about}
+                  name="about"
+                  placeholder={"Tell us about yourself"} 
                   onChange={this.handleChange}
               />
-            <button onClick={this.handleSubmit} >Add Name</button>
+            <button  style={{padding:"5px", border: "solid 3px", borderRadius: "6px", position: "absolute", top: "245px", left: "200px"}}  >Add Name</button>
           </form>
-        
-          <NameBadge />
+     
+          {displayedBadges}
     </div>
       )
     
@@ -88,5 +156,17 @@ class App extends Component{
 
 
 }
+
+// If you want to disable a button when an input string is empty, then the only state you need is the value of the input string.
+
+// const [inputVal, setInputVal] = useState('')
+
+// // ...
+
+// <input value={inputVal} onChange={e => setInputVal(e.target.value)} />
+
+// // ...
+
+// <button disabled={!inputVal}> /* ... */ </button>
 
 export default App;
--- level-3/name-badge/src/NameBadge.js
+++ level-3/name-badge/src/NameBadge.js
@@ -1,15 +1,22 @@
 import React from "react"
 import App from "./App"
+import styles from "./styles.module.css"
 
 function NameBadge (props){
 return(
-    <div>
-        <h1>{props.firstName}</h1>
-        <p>{props.lastName}</p>
-        <p>{props.email}</p>
-        <p>{props.birthPlace}</p>
-        <p>{props.phone}</p>
-        <p>{props.food}</p>
+    <div class={styles.badgeContainer}>
+        <h2 class={styles.badgeHeader}>Badge: </h2>
+        <div class={styles.left}>
+        <p>Name: {props.firstName} {props.lastName}</p>
+        <p>Email: {props.email}</p>
+        <p>Place of Birth: {props.birthPlace}</p>
+        </div>
+        <div class={styles.right}>
+        <p>Phone: {props.phone}</p>
+        <p>Favorite food: {props.food}</p>
+        </div>
+       
+        <div class={styles.about}>{props.about}</div>
 
     </div>
 )
