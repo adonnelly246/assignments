702e3d09616f5523318c1d1e2e812d1dc26ae27a
Alexandra Donnelly <76441969+adonnelly246@users.noreply.github.com>
2021-06-24 13:01:53 -0500

    Add files via upload
    created image and numbers as an object and passed them down

--- level-3/rolling-dice/src/App.js
+++ level-3/rolling-dice/src/App.js
@@ -1,63 +1,81 @@
 import React, {Component} from "react"
 import DiceBox from "./DiceBox"
-import styles from "./styles.module.css"
+import styles from "./styles.css"
 import dice1 from "./images/dice1.jpg"
+import dice2 from "./images/dice2.jpg"
+import dice3 from "./images/dice3.jpg"
+import dice4 from "./images/dice4.jpg"
+import dice5 from "./images/dice5.jpg"
+import dice6 from "./images/dice6.jpg"
 
-///images and assign them to the numbers
-//bonus-click dice and have it stay
+
 class App extends Component{
 
     state ={
-        dice: [0,0,0,0,0,0],
-        image: null
+        dice: [
+            {num: 0, image: ""},
+            {num: 0, image: ""},
+            {num: 0, image: ""},
+            {num: 0, image: ""},
+            {num: 0, image: ""},
+            {num: 0, image: ""}],
+        image: '',
+        imageAlt: ''
     }
   //roll dice, store values in state 
     randDiceRoll() {
         let number = Math.floor(Math.random() * (6)) + 1
-
+        let image = ""
+        // let newDice = this.state.dice.map(obj => {
+        //     if(number === 1) {
+        //         return {image: dice1}
+        //     }
+        // })
+        
         if(number === 1){
-            //display dice1
-            this.state.image = dice1
-            return number + this.state.image
+            image = dice1
         }
         else if(number===2){
-            //display dice2
+            image = dice2
         }
         else if(number===3){
-            //display dice3
+          image= dice3
         }
         else if(number===4){
-            //display dice4
+          image= dice4   
         }
         else if(number===5){
-            //display dice5
+          image= dice5    
         }
         else {
-            //display dice6
+          image= dice6
         }
 
-        return number
+        return {num: number, image: image}
     }
 
     handleClick = () => {
         this.setState(prevState => {    
-            const diceRoll =  prevState.dice.map(num => this.randDiceRoll())                                 
+            const diceRoll =  prevState.dice.map(obj => this.randDiceRoll() )                                 
              
              console.log(diceRoll)
                 return { 
-                   dice: diceRoll
+                   dice: diceRoll,
+                   image:this.state.image,
+                   imageAlt:this.state.imageAlt
                 }
             })
     }
 
     
     render() {
-        const roll =  this.state.dice.map((num) =>{ return <DiceBox key={this.state.id} dice={num}/>})  
+        const roll =  this.state.dice.map((obj) =>{ return <DiceBox key={this.state.id} dice={obj.num} image={obj.image}/>})  
+       
         return(
             
     <div>      
        <h2> 
-           {roll}
+           {roll} 
        </h2>
      
             <button className={styles.button} onClick={this.handleClick}>Roll The Dice!</button>
--- level-3/rolling-dice/src/DiceBox.js
+++ level-3/rolling-dice/src/DiceBox.js
@@ -1,6 +1,6 @@
 import React from "react"
 import App from "./App"
-import styles from "./styles.module.css"
+import styles from "./styles.css"
 
 function DiceBox(props){
     return(
