4dd1b28724cdef1a9870c700fdb323c50773fe82
Alexandra Donnelly <76441969+adonnelly246@users.noreply.github.com>
2021-07-02 11:45:17 -0500

    initial commit

---
+++ level-4/changing-themes/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import App from "./App"
+
+/*You must:
+Provide a Dark and a Light theme
+Have at least 3 display components:
+ex: Navbar, Footer, Main
+Create a ThemeProvider component that manages the themes using Context. */
