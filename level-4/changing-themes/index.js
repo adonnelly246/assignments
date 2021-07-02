d7b6b1ff192f93d2b9e45e4c22308176df8d8c07
Alexandra Donnelly <76441969+adonnelly246@users.noreply.github.com>
2021-07-02 11:47:09 -0500

    Delete level-4/changing-themes directory

--- level-4/changing-themes/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import App from "./App"
-
-/*You must:
-Provide a Dark and a Light theme
-Have at least 3 display components:
-ex: Navbar, Footer, Main
-Create a ThemeProvider component that manages the themes using Context. */
