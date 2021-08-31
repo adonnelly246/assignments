d6e1fb5f44197212f96f137a193fda6d2a54bb25
Alexandra Donnelly <adonnelly246@gmail.com>
2021-08-31 10:33:59 -0500

    checked for errors and cleaned up code

--- level-5/todo-express/routes/routeOne.js
+++ level-5/todo-express/routes/routeOne.js
@@ -1,28 +1,73 @@
 const express = require("express")
 const RouteOne = express.Router()
 const { v4: uuid } = require('uuid');
-const { __esModule } = require("uuid/dist/v1");
+
 
 
 const todos = [
 {
-    name: "The name",
-    description: "The description of the todo",
+    name: "Laundry",
+    description: "wash and dry the laundry",
     imageUrl: "http://www.myimage....",
     completed: false,
     id: uuid()
 },
 
 {
-    name: "The name",
-    description: "The description of the todo",
+    name: "Dry cleaning",
+    description: "pick up dry cleaning",
     imageUrl: "http://www.myimage....",
     completed: false,
+    id: uuid()
+},
+
+{
+    name: "Wash dogs",
+    description: "give the dogs a bath and brush them",
+    imageUrl: "http://www.myimage....",
+    completed: true,
     id: uuid()
 },
 
 ]
 
+//GET all
+RouteOne.get('/', (req, res, next)=>{
+    res.send(todos)
+})
 
+//GET one
+RouteOne.get("/:todoId", (req, res)=>{
+    const todoId = req.params.tvshowId
+    const foundTodo = todos.find(item=> item.id === todoId)
+    res.send(foundTodo)
+})
+
+//POST
+RouteOne.post( "/", (req, res)=>{
+    const newItem = req.body
+    newItem._id = uuid()
+    todos.push(newItem)
+    res.send(`successfully added ${newItem.name}!`)
+})
+
+
+//DELETE 
+RouteOne.delete("/:todoId", (req,res)=>{     
+    const todoId = req.params.todoId
+    const todoIndex = todos.findIndex(item=> item.id === todoId)
+    todos.splice(todoIndex, 1)
+    res.send(`Successfully removed!`)
+}) 
+
+//UPDATE
+RouteOne.put("/:todoId", (req,res)=>{
+    const todoId = req.params.todoId
+    const todoIndex = todos.findIndex(item=> item.id === todoId)
+    const updateObject = req.body
+
+    const updatedTodo = Object.assign(todos[todoIndex], updateObject)
+       res.send(updatedTodo)
+})
 
 module.exports = RouteOne
--- level-5/todo-express/server.js
+++ level-5/todo-express/server.js
@@ -1,2 +1,14 @@
 const express = require("express") 
 const app = express()  
+
+app.use(express.json()) 
+
+
+//Routes//
+app.use("/todos", require("./routes/routeOne.js"))
+
+
+
+app.listen(8000, ()=>{
+    console.log("The server is running on port 8000")
+  })
